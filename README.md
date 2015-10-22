1e85cb149f2f6c7024d05e9c1be8b9abf5d2e082
Alan Plum <me@pluma.io>
2015-10-22 14:16:26 +0200

    Update for 2.7+

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-# Foxx Sessions Example App
+# Foxx Sessions Example
 
-This is the official example app for the Foxx `activateSessions` feature ArangoDB 2.3.
+This is the official example service for local Foxx sessions in ArangoDB 2.
 
-See the source of `index.js` for more details. The `activateSessions` feature is documented in the Sessions section of the Foxx chapter of the ArangoDB 2.3 documentation. The built-in users, sessions and simple-auth apps are documented in the chapter on Foxx Bundled Apps.
+See the source of `index.js` for more details. The `activateSessions` feature is documented in the Sessions section of the Foxx chapter of the ArangoDB documentation starting with ArangoDB 2.3.
 
-If you want to mount this app and use the OAuth2 examples, make sure to mount the [official OAuth2 app](https://github.com/arangodb/foxx-oauth2) at `/oauth2`.
+## Dependencies
+
+This Foxx uses the following dependencies:
+
+* `users`: [util-users-local version 3.0.0](https://github.com/arangodb-foxx/util-users-local)
+* `auth`: [util-simple-auth version 2.0.0](https://github.com/arangodb-foxx/util-simple-auth)
+* `github` (optional): [util-oauth version 2.0.0](https://github.com/arangodb-foxx/util-oauth2) configured for GitHub
+* `google` (optional): [util-oauth version 2.0.0](https://github.com/arangodb-foxx/util-oauth2) configured for Google
+* `facebook` (optional): [util-oauth version 2.0.0](https://github.com/arangodb-foxx/util-oauth2) configured for Facebook
+
+You need to install these services and provide their mount paths in the dependency setup dialog of this example service.
--- index.js
+++ index.js
@@ -1,46 +1,47 @@
 'use strict';
 var Foxx = require('org/arangodb/foxx');
 var controller = new Foxx.Controller(applicationContext);
+var createError = require('http-errors');
 var Credentials = require('./models/credentials');
 var UserProfile = require('./models/userProfile');
 var url = require('url');
 var joi = require('joi');
 
-// As of ArangoDB 2.5 other Foxx apps can be imported directly.
-var auth = Foxx.requireApp('/_system/simple-auth').auth;
-var users = Foxx.requireApp('/_system/users').userStorage;
-var oauth2 = Foxx.requireApp('/oauth2').providers;
+var auth = applicationContext.dependencies.auth;
+var users = applicationContext.dependencies.users;
+var oauth2 = {
+  github: applicationContext.dependencies.github,
+  google: applicationContext.dependencies.google,
+  facebook: applicationContext.dependencies.facebook
+};
 
 function getBaseUrl(req) {
   return url.format({
     protocol: req.protocol,
     hostname: req.server.address,
     port: req.server.port,
-    pathname: '/_db/' + encodeURIComponent(req.database) + applicationContext.mount
+    pathname: `/_db/${encodeURIComponent(req.database)}${applicationContext.mount}`
   });
 }
 
 controller.activateSessions({
-  sessionStorageApp: '/_system/sessions',
-  // This is the default value. ArangoDB always mounts a copy of the built-in sessions app
+  autoCreateSession: true,
+  sessionStorage: '/_system/sessions',
+  // This is the default value. ArangoDB always mounts a copy of the built-in session storage
   // at this mount point, but we could provide the mount point of our own sessions implementation
-  // if it is API compatible. Note that all apps using the same `sessionStorageApp` will share
+  // if it is API compatible. Note that all services using the same `sessionStorage` will share
   // their sessions, so you probably want to mount your own copy.
-  type: 'cookie',
   cookie: {
-    name: 'sid',
-    // The session ID will be stored in a cookie called 'sid'. The cookie name is irrelevant if
-    // our app doesn't need to access it directly. This is the default value and thus optional.
+    name: `${applicationContext.mount}:sid`,
+    // The session ID will be stored in a cookie called. The cookie name is irrelevant if
+    // our service doesn't need to access it directly. This is the default value and thus optional.
     secret: 'secret'
     // If we provide a cookie.secret, ArangoDB will automatically sign the session cookie
     // cryptographically using the given secret. This should be a non-guessable value unique
-    // to this app. The signature will be stored in a cookie with called `name + '.sig'`,
-    // e.g. in this app it would be called 'sid.sig'. Signed cookies will only be read by
+    // to this service. The signature will be stored in a cookie with called `name + '.sig'`,
+    // e.g. in this service it would be called 'sid.sig'. Signed cookies will only be read by
     // ArangoDB if the signature is valid.
   }
-  // Note that unlike `activateAuthentication` the `activateSessions` API doesn't allow
-  // modifying the session TTL (the time to live, or duration). Configuration options like
-  // these must now be set when mounting the sessions app you want to use.
 });
 
 function NotAnAdmin() {}
@@ -60,82 +61,68 @@ function isAdmin(req) {
 controller.post('/login', function(req, res) {
   var credentials = req.params('credentials');
   var user = users.resolve(credentials.get('username'));
-  // The built-in users app provides a `resolve` method that fetches the user object
+
+  // The users-local service provides a `resolve` method that fetches the user object
   // for a given username, or `null` if the user could not be found.
   var valid = auth.verifyPassword(
     user ? user.get('authData').simple : {},
     credentials.get('password')
   );
-  // The built-in simple-auth app provides a `verifyPassword` method we can use to
+
+  // The simple-auth service provides a `verifyPassword` method we can use to
   // determine if the entered password matches the stored user's password. It expects
   // an auth object containing the user's salt and password hash, and the password
   // to verify. Because we don't want to distinguish between invalid passwords and
   // invalid usernames, we just pass in an empty object if we couldn't find a user.
-  if (valid) {
-    req.session.get('sessionData').username = user.get('user');
-    // This is an example of storing arbitrary application-specific data in the
-    // `sessionData` property of the session object. In this case, the username.
-    req.session.setUser(user);
-    // The `setUser` method copies the user's `_id` and `userData` properties into
-    // the session object. This allows us to determine whether a user is logged in
-    // by checking whether the session's `uid` property is empty.
-    req.session.save();
-    // Whenever we modify the session object, we need to explicitly save it to make
-    // sure the data is updated in the database. We don't need to worry about setting
-    // the session cookie as it will be renewed automatically when we are using the
-    // `activateSessions` feature.
-    res.json({success: true, user: user.get('userData'), username: user.get('user')});
-  } else {
-    res.status(403);
-    res.json({success: false, error: 'Invalid password or unknown username.'});
+  if (!valid) {
+    throw createError(403, 'Invalid password or unknown username.');
   }
+
+  req.session.get('sessionData').username = user.get('user');
+  // This is an example of storing arbitrary service-specific data in the
+  // `sessionData` property of the session object. In this case, the username.
+  req.session.setUser(user);
+  // The `setUser` method copies the user's `_id` and `userData` properties into
+  // the session object. This allows us to determine whether a user is logged in
+  // by checking whether the session's `uid` property is empty.
+  req.session.save();
+  // Whenever we modify the session object, we need to explicitly save it to make
+  // sure the data is updated in the database. We don't need to worry about setting
+  // the session cookie as it will be renewed automatically when we are using the
+  // `activateSessions` feature.
+  res.json({success: true, user: user.get('userData'), username: user.get('user')});
 })
 .bodyParam('credentials', {
   description: 'Authentication credentials.',
   type: Credentials
-})
-.summary('Authenticate')
-.notes('Attempts to log the user in with username and password.');
+});
 
 /** Registration route
  *
- * Demonstrates creating a new account.
+ * Creates a new user and logs the user in.
  */
 controller.post('/register', function(req, res) {
   var credentials = req.params('credentials');
   if (credentials.get('username').indexOf(':') !== -1) {
-    // This is an example of arbitrary application-specific restrictions.
+    // This is an example of arbitrary service-specific restrictions.
     // We want to use the colon as a special character for usernames elsewhere
-    // in this app, so we can't allow users to create accounts with that
+    // in this service, so we can't allow users to create accounts with that
     // character in it.
-    res.status(400);
-    res.json({
-      success: false,
-      error: 'Username must not contain a colon'
-    });
-    return;
+    throw createError(400, 'Username must not contain a colon.');
   }
+  var username = credentials.get('username');
   var user;
   try {
-    user = users.create(
-      credentials.get('username'),
-      req.params('profile').forDB(),
-      {
-        simple: auth.hashPassword(credentials.get('password'))
-      }
-    );
-    // The built-in users app provides a `create` method to create users.
+    user = users.create(username, req.params('profile').forDB(), {
+      simple: auth.hashPassword(credentials.get('password'))
+    });
+    // The users-local service provides a `create` method to create users.
     // The second parameter will be stored as the user object's `userData`
     // property, the last parameter will be stored as its `authData`.
     // Generally a user's `authData` should never leave the database.
   } catch (err) {
     if (err instanceof users.errors.UsernameAlreadyTaken) {
-      res.status(400);
-      res.json({
-        success: false,
-        error: 'Username already taken'
-      });
-      return;
+      throw createError(400, `Username "${username}" already in use.`);
     }
     throw err;
   }
@@ -157,9 +144,7 @@ controller.post('/register', function(req, res) {
 .bodyParam('profile', {
   description: 'User profile data', 
   type: UserProfile
-})
-.summary('Register')
-.notes('Create a new account and log the user in.');
+});
 
 /** Logout route
  *
@@ -171,66 +156,60 @@ controller.destroySession('/logout', function(req, res) {
   // (by default) creates a new session or alternatively (only when using cookie
   // sessions) clears the session cookie.
   res.json({success: true});
-})
-.summary('De-authenticate')
-.notes('Wipes the session data for the active session.');
+});
 
-/** OAuth2 redirect
+/** OAuth2 authorization redirect
  *
- * Redirects the user to the authorization endpoint of the given provider.
+ * Redirects the user to the authorization endpoint of the given OAuth2 provider.
  */
 controller.post('/oauth2/:provider/auth', function(req, res) {
-  if (!oauth2) {
-    res.status(500);
-    res.json({success: false, error: "Expecting 'oauth2' provider to be available at mount point '/oauth2'. Please execute: require('org/arangodb/foxx/manager').install('oauth2', '/oauth2')."});
-    return;
+  var providerName = req.urlParameters.provider;
+  var provider = oauth2[providerName];
+  if (!provider) {
+    throw createError(500, `OAuth2 provider "${providerName}" not found or not configured correctly.`);
   }
-  // This app expects the official OAuth2 app available at
-  // https://github.com/arangodb/foxx-oauth2 to be mounted at `/oauth2`.
-
-  var provider = oauth2.get(req.urlParameters.provider);
   res.status(303);
   res.set('location', provider.getAuthUrl(
     getBaseUrl(req) + '/api/oauth2/' +
-    provider.get('_key') + '/login',
+    providerName + '/login',
     {state: req.session.get('_key')}
   ));
 })
-.pathParam('provider', joi.string().description('Provider _key.'))
-.summary('OAuth2 authorization redirect')
-.notes('Redirects to the authorization endpoint of an OAuth2 provider.');
+.pathParam('provider', joi.string().description('Provider _key.'));
 
 
-/** OAuth2 callback
+/** OAuth2 authorization callback
  *
- * An example for an OAuth2 callback.
+ * Redirect target for the OAuth2 authorization endpoint.
  */
 controller.get('/oauth2/:provider/login', function(req, res) {
-  var provider = oauth2.get(req.urlParameters.provider);
+  var providerName = req.urlParameters.provider;
+  var provider = oauth2[providerName];
+  if (!provider) {
+    throw createError(500, `OAuth2 provider "${providerName}" not found or not configured correctly.`);
+  }
   if (req.params('error')) {
-    res.status(500);
-    res.json({success: false, error: req.params('error')});
+    throw createError(500, req.params('error'));
     return;
   }
   if (req.params('state') !== req.session.get('_key')) {
-    res.status(400);
-    res.json({success: false, error: 'CSRF mismatch'});
+    throw createError(400, 'CSRF mismatch');
     return;
   }
   try {
     var authData = provider.exchangeGrantToken(
       req.params('code'),
-      getBaseUrl(req) + '/api/oauth2/' + provider.get('_key') + '/login'
+      getBaseUrl(req) + '/api/oauth2/' + providerName + '/login'
     );
     var profile = provider.fetchActiveUser(authData.access_token);
-    var username = provider.get('_key') + ':' + provider.getUsername(profile);
+    var username = providerName + ':' + provider.getUsername(profile);
     // We want user objects created via OAuth2 to have their usernames prefixed with
-    // the `_key` of the OAuth2 provider and a colon. This allows us to distinguish
+    // the name of the OAuth2 provider and a colon. This allows us to distinguish
     // them more easily.
     var user = users.resolve(username);
     if (!user) user = users.create(username);
-    user.get('userData')['oauth2_' + provider.get('_key')] = profile;
-    user.get('authData')['oauth2_' + provider.get('_key')] = authData;
+    user.get('userData')['oauth2_' + providerName] = profile;
+    user.get('authData')['oauth2_' + providerName] = authData;
     user.save();
     req.session.get('sessionData').username = user.get('user');
     req.session.setUser(user);
@@ -238,40 +217,36 @@ controller.get('/oauth2/:provider/login', function(req, res) {
     res.status(303);
     res.set('location', getBaseUrl(req) + '/');
   } catch(err) {
-    res.status(500);
-    res.json({success: false, error: err.message});
+    throw createError(500, err.message);
   }
 })
-.pathParam('provider', joi.string().description('Provider _key.'))
-.summary('OAuth2 authorization callback')
-.notes('Redirect target for the OAuth2 authorization endpoint.');
+.pathParam('provider', joi.string().description('Provider _key.'));
 
 /** Registered user list
  *
- * Demonstrates fetching the list of usernames from the user storage.
+ * Returns a list of all known usernames.
  */
 controller.get('/users', function(req, res) {
   res.json({users: users.list()});
-})
-.summary('Registered users')
-.notes('Returns a list of all known usernames.');
+});
 
 /** Get current user
  *
- * If the user is logged in, returns the user's userData. Otherwise
+ * If the user is logged in, returns the active user's userData. Otherwise
  * returns null for the userData.
  */
 controller.get('/whoami', function(req, res) {
   if (!req.session.get('uid')) {
     res.json({user: null, username: ''});
   } else {
-    res.json({user: req.session.get('userData') || {}, username: req.session.get('sessionData').username});
+    res.json({
+      user: req.session.get('userData') || {},
+      username: req.session.get('sessionData').username
+    });
   }
-})
-.summary('Session user status')
-.notes('Returns the active user or null.');
+});
 
-/** Counter
+/** Get and increment a counter
  *
  * Demonstrates storing and updating a value in the session.
  * Also shows how to restrict a route to logged-in users.
@@ -284,9 +259,7 @@ controller.get('/counter', function(req, res) {
   req.session.save();
   res.json({counter: sdata.counter});
 })
-.onlyIfAuthenticated()
-.summary('Get and increment a counter')
-.notes('Fetches the number of times this routes has been called in this session.');
+.onlyIfAuthenticated();
 
 /** Session dump
  *
@@ -297,6 +270,5 @@ controller.get('/dump', function(req, res) {
   res.json(req.session.forClient());
 })
 .onlyIf(isAdmin)
-.errorResponse(NotAnAdmin, 403, 'You are not an admin.')
-.summary('Dump session object')
-.notes('Returns the session object. Demonstrates restricting route access.');
+.errorResponse(NotAnAdmin, 403, 'You are not an admin.');
+
